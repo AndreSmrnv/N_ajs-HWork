f66fca9d249e29df405024355050077b0370429c
AndreSmrnv <tehmir@bk.ru>
2021-03-01 23:02:32 +0300

    hWork5_2 - add levelUp and damage

---
+++ 5_2/src/js/__tests__/app.test.js
@@ -0,0 +1,112 @@
+// import {
+//   Character, Bowman, Swordsman, Magician, Undead, Zombie, Daemon,
+// } from '../app';
+
+import Character from '../Character';
+import Bowman from '../Bowman';
+import Swordsman from '../Swordsman';
+import Magician from '../Magician';
+import Daemon from '../Daemon';
+import Undead from '../Undead';
+import Zombie from '../Zombie';
+
+test('Character name lenght 1', () => {
+  const result = new Character('test', 'Bowman');
+  expect(() => { result.name = '1'; }).toThrow(Error);
+});
+test('Character name lenght 2', () => {
+  const result = new Character('22', 'Bowman');
+  expect(result.name).toBe('22');
+});
+test('Character name lenght 10', () => {
+  const result = new Character('1010101010', 'Bowman');
+  expect(result.name).toBe('1010101010');
+});
+test('Character name lenght 11', () => {
+  const result = new Character('test', 'Bowman');
+  expect(() => { result.name = '11111111111'; }).toThrow(Error);
+});
+test('Character health', () => {
+  const result = new Character('test', 'Bowman');
+  expect(result.health).toBe(100);
+});
+test('Character level', () => {
+  const result = new Character('test', 'Bowman');
+  expect(result.level).toBe(1);
+});
+test('Character not correct Type', () => {
+  const result = new Character('test', 'Bowman');
+  expect(() => { result.type = 'bad'; }).toThrow(Error);
+});
+test('Character name get', () => {
+  const result = new Character('1010101010', 'Bowman');
+  expect(result._name).toBe('1010101010');
+});
+test('Character type get', () => {
+  const result = new Character('1010101010', 'Bowman');
+  const exp = result._type;
+  expect(result.type).toBe(exp);
+});
+
+test('Bowman attack', () => {
+  const result = new Bowman('test', 'Bowman');
+  expect(result.attack).toBe(25);
+});
+test('Bowman defence', () => {
+  const result = new Bowman('test', 'Bowman');
+  expect(result.defence).toBe(25);
+});
+test('Swordsman attack', () => {
+  const result = new Swordsman('test', 'Bowman');
+  expect(result.attack).toBe(40);
+});
+test('Swordsman defence', () => {
+  const result = new Swordsman('test', 'Bowman');
+  expect(result.defence).toBe(10);
+});
+test('Magician attack', () => {
+  const result = new Magician('test', 'Bowman');
+  expect(result.attack).toBe(10);
+});
+test('Magician defence', () => {
+  const result = new Magician('test', 'Bowman');
+  expect(result.defence).toBe(40);
+});
+test('Undead attack', () => {
+  const result = new Undead('test', 'Bowman');
+  expect(result.attack).toBe(25);
+});
+test('Undead defence', () => {
+  const result = new Undead('test', 'Bowman');
+  expect(result.defence).toBe(25);
+});
+test('Zombie attack', () => {
+  const result = new Zombie('test', 'Bowman');
+  expect(result.attack).toBe(40);
+});
+test('Zombie defence', () => {
+  const result = new Zombie('test', 'Bowman');
+  expect(result.defence).toBe(10);
+});
+test('Daemon attack', () => {
+  const result = new Daemon('test', 'Bowman');
+  expect(result.attack).toBe(10);
+});
+test('Daemon defence', () => {
+  const result = new Daemon('test', 'Bowman');
+  expect(result.defence).toBe(40);
+});
+
+test('Bowman levelUp', () => {
+  const result = new Bowman('test', 'Bowman');
+  result.levelUp();
+  expect(result.level).toBe(2);
+  expect(result.attack ).toBe(45);
+  expect(result.defence).toBe(45);
+  expect(result.health ).toBe(100);
+});
+test('Bowman damage', () => {
+  const result = new Bowman('test', 'Bowman');
+  result.damage(100);  
+  expect(result.health ).toBe(25);
+});
