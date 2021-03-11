a5b828517e4b7654db2e4a46e00cf84c8145340c
AndreSmrnv <tehmir@bk.ru>
2021-03-11 23:21:07 +0300

    eslint fix

--- 9_2/src/js/app.js
+++ 9_2/src/js/app.js
@@ -3,17 +3,13 @@
 import getBuffer from './getbuffer';
 
 export class ArrayBufferConverter {
-    constructor() {
-        this.ArrayBuffer ;
-      }
   load(buffer) {
-    this.ArrayBuffer  = getBuffer(buffer);
+    this.ArrayBuffer = getBuffer(buffer);
   }
 
   toString() {
-    return String.fromCharCode(...new Uint16Array(this.ArrayBuffer ));
+    return String.fromCharCode(...new Uint16Array(this.ArrayBuffer));
   }
 }
-
 
 console.log('worked');
