9f670cb72d2602de5e7e5e4b365586d157acb41d
AndreSmrnv <tehmir@bk.ru>
2021-06-12 10:25:10 +0300

    add jest test

---
+++ 10_2/src/js/__tests__/gamesavingloader.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+import GameSavingLoader from '../gamesavingloader';
+
+it('Check read data {...}', async () => {
+  const dataCheck = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+  const data = await GameSavingLoader.load();
+  expect(data).toEqual(dataCheck);
+});
