eee6b24b88793cb27d557d0deaaae8c863d09b39
AndreSmrnv <tehmir@bk.ru>
2021-06-12 14:39:25 +0300

    add jest test

--- 10_1/src/js/Reader.js
+++ 10_1/src/js/Reader.js
@@ -1,7 +1,7 @@
 function read() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
+      const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
       return ((input) => {
         const buffer = new ArrayBuffer(input.length * 2);
         const bufferView = new Uint16Array(buffer);
